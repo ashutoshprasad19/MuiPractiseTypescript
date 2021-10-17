f5405bdb5dd0442e29463ebe673cdcbb5c5ca81c
ashu <ashuyash123@gmail.com>
2021-10-17 20:13:55 +0530

    added some changes for next js functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import Link from '/next/Link';
 
 function App() {
   return (
+    <React.Fragment>
    <Button variant = "contained" > Hello World </Button>
+   <Link href="/">
+     <a>Home</a>
+   </Link>
+   </React.Fragment>
       
   );
 }
